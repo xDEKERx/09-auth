e4c89e0d8354f99976d487f2bbba344990fce990
Dmytro Deker <prostodeker@gmail.com>
2025-07-17 12:57:42 +0300

    V6

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,18 +1,5 @@
-.spinner {
-    width: 48px;
-    height: 48px;
-    border: 6px solid #ccc;
-    border-top-color: #FFFF00;
-    border-left-color: #1E90FF;
-    border-bottom-color: #FFFF00;
-    border-right-color: #1E90FF;
-    border-radius: 50%;
-    animation: spin 0.8s linear infinite;
-    margin: 40px auto;
-}
+import css from "./Loader.module.css";
 
-@keyframes spin {
-    to {
-        transform: rotate(360deg);
-    }
+export default function Loader() {
+  return <div className={css.spinner}></div>;
 }
