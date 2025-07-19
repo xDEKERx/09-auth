ba49198a578c34c886493cd51de145db35c4dc0c
Dmytro Deker <prostodeker@gmail.com>
2025-07-19 20:19:28 +0300

    fix

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,7 +1,7 @@
 import { Note } from "../../types/note";
 import { NewNote } from "../../types/note";
 
-import { serverApi } from "../api/api";
+import { clientApi } from "./api"; 
 
 import {
   RegisterRequest,
@@ -23,76 +23,63 @@ export const fetchNotes = async (
 ): Promise<NotesHttpResponse> => {
   const PARAMS = new URLSearchParams({
     ...(query !== "" ? { search: query } : {}),
-
     ...(tag !== undefined ? { tag } : null),
     page: page.toString(),
   });
 
-  const response = await serverApi.get<NotesHttpResponse>("/notes", {
+  const response = await clientApi.get<NotesHttpResponse>("/notes", { 
     params: PARAMS,
   });
 
   return response.data;
 };
 
-// POST FETCH
-
 export const createNote = async (newNote: NewNote): Promise<Note> => {
-  const response = await serverApi.post("/notes", newNote);
+  const response = await clientApi.post("/notes", newNote); 
   return response.data;
 };
-
-// FETCH NOTE BY ID
 
 export const fetchNoteById = async (id: string): Promise<Note> => {
-  const response = await serverApi.get<Note>(`/notes/${id}`);
+  const response = await clientApi.get<Note>(`/notes/${id}`); 
   return response.data;
 };
 
-// DELETE POST
-
 export const deleteNote = async (id: string): Promise<Note> => {
-  const response = await serverApi.delete<Note>(`/notes/${id}`);
+  const response = await clientApi.delete<Note>(`/notes/${id}`); 
   return response.data;
 };
 
 // REGISTER
-
-export const register = async (data: RegisterRequest) => {
-  const res = await serverApi.post<User>("/auth/register", data);
+export const register = async (data: RegisterRequest): Promise<User> => {
+  const res = await clientApi.post<User>("/auth/register", data); 
   return res.data;
 };
 
 // LOGIN
-
-export const login = async (data: LoginRequest) => {
-  const res = await serverApi.post<User>("/auth/login", data);
+export const login = async (data: LoginRequest): Promise<User> => {
+  const res = await clientApi.post<User>("/auth/login", data); 
   return res.data;
 };
 
 // PATCH
-
-export const editProfile = async (data: UpdateUserRequest) => {
-  const res = await serverApi.patch<User>("/users/me", data);
+export const editProfile = async (data: UpdateUserRequest): Promise<User> => {
+  const res = await clientApi.patch<User>("/users/me", data);
   return res.data;
 };
 
 // CHECK SESSION
-
-export const checkSession = async () => {
-  const res = await serverApi.get<CheckSessionRequest>("/auth/session");
+export const checkSession = async (): Promise<boolean> => {
+  const res = await clientApi.get<CheckSessionRequest>("/auth/session"); 
   return res.data.success;
 };
 
 // AUTH ME
-
 export const getMe = async (): Promise<User> => {
-  const { data } = await serverApi.get<User>("/users/me");
+  const { data } = await clientApi.get<User>("/users/me"); 
   return data;
 };
 
 // LOGOUT
-
 export const logout = async (): Promise<void> => {
-  await serverApi.post("/auth/logout");
+  await clientApi.post("/auth/logout"); 
 };
